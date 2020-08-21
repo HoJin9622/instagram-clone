294ebdbf199c200549459eb9147aeea8ff766e29
hojin9622 <kiss0104040@gmail.com>
2020-08-22 00:09:01 +0900

    Create Header

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
-      <h1>
-        Hello Clever Programmers Lets' build an Instagram Clone with React
-      </h1>
-
-      <div className="app__header"></div>
-      {/* Header */}
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
+      </div>
 
       {/* Post */}
       {/* Post */}
